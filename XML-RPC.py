b5cab2ab0f9e3cafc2eebdbdffab883144109726
Michael <jixian.79@outlook.com>
2017-07-13 17:13:01 +0800

    用异常代替错误返回代码

--- XML-RPC.py
+++ XML-RPC.py
@@ -3,19 +3,32 @@
 
 # In[ ]:
 
-from xmlrpclib import ServerProxy
+from xmlrpclib import ServerProxy,Fault
 from xmlrpclib import Binary
-from os.path import join,isfile
+from os.path import join,isfile,abspath
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 from urlparse import urlparse
 import sys
 
 SimpleXMLRPCServer.allow_reuse_address = 1
+
 MAX_HISTORY_LENTH = 6
 
-OK = 1
-FAIL = 2
-EMPTY = ''
+UNHANDLED = 100
+ACCESS_DENIED = 200
+
+class UnhandledQuery(Fault):
+    """"""
+    def __init__(self,message = "Couldn't handle the query"):
+        Fault.__init__(self,UNHANDLED,message)
+class AccessDenied(Fault)
+    def __init__(self,message = "ACCESS DENIED"):
+        Fault.__init__(self,ACCESS_DENIED,message)
+        
+def inside(pdir,name):
+    pdir = abspath(pdir)
+    name = abspath(name)
+    return name.startswith(join(pdir,''))
 
 def getPort(url):
     '用url中提取端口'
@@ -36,32 +49,27 @@ class Node:
         """
         查询
         """
-	print "Start query"
-        code,data = self._handle(query)
-        if code == OK:
-            return code,data
-        else:
+        print "Start query"
+        try:
+            return self._handle(query)
+        except UnhandledQuery:
             history = history + [self.url]
-            if len(history) >= MAX_HISTORY_LENTH:
-                return FAIL,EMPTY
+            if len(history) >= MAX_HISTORY_LENTH:raise
             return self._broadcast(query,history)
         
     def hello(self,other):
         """用于将节点介绍给其他节点"""
         self.known.add(other)
-        return OK
+        return 0
     
     def fetch(self,query,secret):
         """用于让节点找到文件并下载"""
-        if secret != self.secret: return FAIL
-        code,data = self.query(query)
-        if code == OK:
-            f = open(join(self.dirname,query),'wb')
-            f.write(data.data)
-            f.close()
-            return OK
-        else:
-            return FAIL
+        if secret != self.secret: raise AccessDenied
+        result = self.query(query)
+        f = open(join(self.dirname,query),'wb')
+        f.write(result.data)
+        f.close()
+        return 0
         
     def _start(self):
         """启动服务"""
@@ -73,8 +81,9 @@ class Node:
         """处理请求"""
         dir = self.dirname
         name = join(dir,query)
-        if not isfile(name): return FAIL,EMPTY
-        return OK,Binary(open(name,'rb').read())
+        if not isfile(name): raise UnhandledQuery
+        if not inside(dir,name):raise AccessDenied
+        return Binary(open(name,'rb').read())
     
     def _broadcast(self,query,history):
         """广播"""
@@ -82,12 +91,14 @@ class Node:
             if other in history :continue
             try:
                 s = ServerProxy(other)
-                code,data = s.query(query,history)
-                if code ==OK:
-                    return code,data
+                return s.query(query,history)
+            except Fault,f:
+                if f.faultCode = UNHANDLED:pass
+                else:self.known.remove(other)
             except:
                 self.known.remove(other)
-        return FAIL,EMPTY
+        raise UnhandledQuery
+        
     
 def main():
 	url,directory,secret = sys.argv[1:]
